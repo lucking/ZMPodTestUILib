16ab646e9140ee34c89106ce410da6efe1fa0357
ZM <1531073749@qq.com>
2019-05-17 18:09:38 +0800

    0.1.6
    0.1.6

---
+++ ZMTestKit/UIKitCategroy/ZMTestBaseView.h
@@ -0,0 +1,22 @@
+//
+//  ZMTestBaseView.h
+//  ZMTestKit
+//
+//  Created by ZM on 2019/5/17.
+//  Copyright © 2019 ZM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMTestBaseView : UIView
+
+- (void)test1;
+
++ (void)test2;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZMTestKit/UIKitCategroy/ZMTestBaseView.m
@@ -0,0 +1,31 @@
+//
+//  ZMTestBaseView.m
+//  ZMTestKit
+//
+//  Created by ZM on 2019/5/17.
+//  Copyright © 2019 ZM. All rights reserved.
+//
+
+#import "ZMTestBaseView.h"
+
+@implementation ZMTestBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)test1 {
+    NSLog(@"---ZMTestBaseView= test1 ");
+}
+
++ (void)test2 {
+    NSLog(@"---ZMTestBaseView= test2 ");
+}
+
+
+
+@end
